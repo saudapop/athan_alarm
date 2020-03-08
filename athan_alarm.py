c8029018045444077c73235b257f23f1269c0fa5
Saud Ahmed <saud.ahmed@datarobot.com>
2020-03-08 18:21:52 -0400

    init

---
+++ athan_alarm.py
@@ -0,0 +1,74 @@
+import json
+import datetime
+import time
+import sched
+
+from pygame import mixer
+
+
+def clean_time_string(string: str):
+    return string.replace(' (EST)', '').replace(' (EDT)', '')
+
+
+################### SOUND SETUP ###########################
+mixer.init()
+mixer.music.load('./athan_mp3s/1_alafasy.mp3')
+
+
+def play_athan():
+    mixer.music.play()
+#---------------------------------------------------------#
+
+################### DATE CONSTANTS ########################
+
+
+MONTHS = ['jan', 'feb', 'march',
+          'april', 'may', 'june',
+          'july', 'aug', 'sept',
+          'oct', 'nov', 'dec']
+
+TODAY = datetime.datetime.now()
+MONTH_NUM = TODAY.month
+MONTH_ENG = MONTHS[MONTH_NUM - 1]
+DAY = TODAY.day - 1
+#---------------------------------------------------------#
+
+
+# read today's prayer times
+with open(f'./json/{MONTH_NUM}_{MONTH_ENG}.json') as f:
+    PRAYER_TIMES = json.loads(f.read())
+    DATE = PRAYER_TIMES[DAY]['date']
+    TIMINGS = PRAYER_TIMES[DAY]['timings']
+
+
+# returns [['HH', 'MM (EST)'], ...] a two-dimensional array of times
+parsed_timings = [v.split(':')[:2] for k, v in TIMINGS.items()]
+
+
+# remove the '(EST)' from second list item which has the min
+parsed_timings = [
+    [timing[0], clean_time_string(timing[1])]
+    for timing in parsed_timings
+]
+
+
+time_format = '%d-%m-%Y %H:%M:%S'
+
+for timing in parsed_timings:
+
+    time_in_seconds_since_epoch = time.strptime(
+        f'{foo["date"]} {timing[0]}:{timing[1]}:00',
+        time_format
+    )
+
+    t = time.mktime(t)
+
+    EVENT_HAS_NOT_OCCURED = time.time() - t <= 1
+
+    if EVENT_HAS_NOT_OCCURED:
+        my_sched.enterabs(t, 1, play_athan)
+    else:
+        pass
+
+
+my_sched.run()
---
+++ fetch_prayer_times.py
@@ -0,0 +1,34 @@
+import json
+import os
+import requests
+
+# Get coordiantes from https://www.latlong.net/
+
+LATITUDE =  # fill in your latitude
+LONGITUDE =  # Fill in your longitude
+
+BASE_URL = 'http://api.aladhan.com/v1/calendar?latitude={}&longitude={}&method=2&month={}&year=2020'
+
+
+MONTHS = ['jan', 'feb', 'march',
+          'april', 'may', 'june',
+          'july', 'aug', 'sept',
+          'oct', 'nov', 'dec']
+
+TIMINGS = ["Fajr", "Sunrise", "Dhuhr",
+           "Asr", "Maghrib", "Isha", ]
+
+for i, month in enumerate(MONTHS):
+    response = requests.get(BASE_URL.format(LATITUDE, LONGITUDE, i+1))
+
+    # create list of prayer times with just the values we are interested in
+    PRAYER_TIMES = [
+        {
+            'date': x['date']['gregorian']['date'],
+            'timings': {k: v for k, v in x['timings'].items() if k in TIMINGS}
+        }
+        for x in response.json()['data']
+    ]
+    JSON_OBJECT = json.dumps(PRAYER_TIMES, indent=4)
+    with open('./json/{}_{}.json'.format(i+1, month), "w") as FILE:
+        FILE.write(JSON_OBJECT)
