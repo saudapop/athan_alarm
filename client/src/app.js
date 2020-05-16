9fac65f0d32ec8af468408953039f7f6a5c093a5
Saud Ahmed <saud.ahmed@datarobot.com>
2020-05-16 16:13:47 -0400

    react is working

---
+++ client/src/app.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+export function App() {
+  return (
+    <div>
+      <h1>Athan App Dashboard v0.0.1</h1>
+      👋🌎
+    </div>
+  );
+}
--- client/src/index.js
+++ client/src/index.js
@@ -1,16 +1,21 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import { App } from "./app";
 import axios from "axios";
 
 console.log("assalamualaikum wa rahmutullahi wa barakatuhu");
 
-axios.get("/api/get-preferences/").then((res) => console.log(res));
+// axios.get("/api/get-preferences/").then((res) => console.log(res));
 
-axios
-  .post("/api/update-scheduler/", {
-    Dhuhr: 1,
-    Asr: 1,
-    Maghrib: 1,
-    Fajr: 1,
-    Isha: 1,
-  })
-  .then((res) => console.log(res))
-  .catch((e) => console.log(e));
+// axios
+//   .post("/api/update-scheduler/", {
+//     Dhuhr: 1,
+//     Asr: 1,
+//     Maghrib: 1,
+//     Fajr: 1,
+//     Isha: 1,
+//   })
+//   .then((res) => console.log(res))
+//   .catch((e) => console.log(e));
+
+ReactDOM.render(<App />, document.getElementById("root"));
